90caa4b69bc4fba41567baf87a24c4d163d958a2
LuisFernandoGonzalezCortes <88851274+LuisFernandoGonzalezCortes@users.noreply.github.com>
2021-08-23 22:49:41 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    
+    t=float(input("Dar tiempo en minutos: "))
+
+    c = ((t * 60) * (.57))
+    print("Centimetros recorridos: ", c)
 
 if __name__ == '__main__':
     main()
