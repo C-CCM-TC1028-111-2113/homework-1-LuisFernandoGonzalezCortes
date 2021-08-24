ad454e3adf29342845c2a94b82656ea075b00a48
LuisFernandoGonzalezCortes <88851274+LuisFernandoGonzalezCortes@users.noreply.github.com>
2021-08-24 00:32:29 -0500

    Update exercise.py

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    
+     n=int(input("Dame un numero que no empiece por cero: "))
+
+    if n/2:int
+        print("El numero de digitos pares es: 1"
+    elif
+        print("El numero de digitos pares es: 0"
 
 
 if __name__ == '__main__':
